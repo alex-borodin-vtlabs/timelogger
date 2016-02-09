29141a5b8bf293b01bd7e6c85f853d660499c062
Александр Бородин <aleksandrborodin1@MacBook-Pro-Aleksandr.local>
2016-02-09 06:42:25 +0300

    production mailer fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'shrouded-brushlands-70261.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address: 'smtp.gmail.com',
